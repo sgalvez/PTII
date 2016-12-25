ae516437ac5c6987ec9a004ff2d83aab6fe7d6a6
Sebastian Galvez <sagalvezlopez@gmail.com>
2016-12-25 15:29:13 -0300

    Update 001_init_db.sql

--- mysql/files/001_init_db.sql
+++ mysql/files/001_init_db.sql
@@ -2,7 +2,7 @@ drop database latam;
 create database latam;
 use latam;
 
-CREATE USER 'sgalvez'@'localhost' IDENTIFIED BY 'n03ntr4r';
+CREATE USER 'sgalvez'@'localhost' IDENTIFIED BY 't4st';
 GRANT ALL PRIVILEGES ON *.* TO 'sgalvez'@'localhost' WITH GRANT OPTION;
-CREATE USER 'sgalvez'@'%' IDENTIFIED BY 'n03ntr4r';
+CREATE USER 'sgalvez'@'%' IDENTIFIED BY 't4st';
 GRANT ALL PRIVILEGES ON *.* TO 'sgalvez'@'%' WITH GRANT OPTION;
