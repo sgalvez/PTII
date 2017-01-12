43435f978815edd3ad1669df3203564f7af743d4
Sebastian Galvez <sgalvez@debian-master>
2017-01-12 03:23:50 +0000

    Fix

--- frontApp/public/hello.js
+++ frontApp/public/hello.js
@@ -1,7 +1,6 @@
 angular.module('demo', [])
 .controller('Hello', function($scope, $http) {
-  //  $http.get('http://vdocker:8000/airports/getCity?airportCode=SCL').
- $http.get('http://rest-service.guides.spring.io/greeting').
+    $http.get('http://localhost:8000/airports/getCity?airportCode=SCL').
         then(function(response) {
             $scope.greeting = response.data;
         });
