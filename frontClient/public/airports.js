f26d9b37fc0cb0c92ee50e1ed03ca81a78ac6ffd
Sebastian Antonio Galvez Lopez <sebastian.galvez@latam.com>
2017-01-12 11:34:58 -0300

    Fix

--- frontClient/public/airports.js
+++ frontClient/public/airports.js
@@ -1,8 +1,7 @@
 angular.module('demo', [])
-.controller('Hello', function($scope, $http) {
+.controller('Airports', function($scope, $http) {
     $http.get('http://vdocker1:8000/airports?apikey=87b9175460234cb3a941f559d37f5667').
         then(function(response) {
-            //$scope.greeting = response.data;
             angular.forEach(response.data._embedded.airports, function(value, key) {
                 console.log(value.cityname);
                 angular.element(document.querySelector('#airport-select')).append("<option value="+value.airportcode+">"+value.cityname+"</option>");
