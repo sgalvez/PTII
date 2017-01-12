c13806352876c21ce233744dad2a7aee4d288e1b
Sebastian Galvez <sagalvezlopez@gmail.com>
2017-01-12 10:07:43 -0300

    Refactor

--- frontApp/public/hello.js
+++
@@ -1,7 +0,0 @@
-angular.module('demo', [])
-.controller('Hello', function($scope, $http) {
-    $http.get('http://localhost:8000/airports/getCity?airportCode=SCL').
-        then(function(response) {
-            $scope.greeting = response.data;
-        });
-});
