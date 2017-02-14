9ce0518c494c79526efa5d5dc99f73caf1476f99
Sebastian Galvez <sagalvezlopez@gmail.com>
2017-02-14 05:18:45 -0300

    Refactor

---
+++ apiPortal/bin/kong-dashboard.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+var dashboard = require('../lib/kong-dashboard');
+var parseArgs = require('minimist');
+var argv = parseArgs(process.argv.slice(2));
+
+// validar opciones
+var validOptions = ['_', 'a', 'p'];
+function hasInvalidOptions (argv) {
+    var isInvalid = false;
+    Object.keys(argv).some(function (optionName) {
+        if (validOptions.indexOf(optionName) < 0) {
+            isInvalid = true;
+            return true;
+        }
+    });
+    return isInvalid;
+}
+
+// Ayuda
+var validCommands = ['start', 'build']
+if (argv.help || hasInvalidOptions(argv) || validCommands.indexOf(argv._[0]) < 0) {
+    console.log("Usage:");
+    console.log(" * kong-dashboard build");
+    console.log(" * kong-dashboard start [-p 8080] [-a user=password]");
+    process.exit();
+}
+
+if (argv._[0] === 'build') {
+    dashboard.build();
+}
+if (argv._[0] === 'start') {
+    var port = argv.p ? argv.p : 8080;
+    var auth = argv.a;
+    dashboard.serve(port, auth);
+}
